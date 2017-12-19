9b57add46f634fa933ba48a2e0dff345c4ae4bb3
bgvladedivac <bgvladedivac@gmail.com>
2017-12-19 14:09:10 +0200

    Longest Word.

---
+++ Easy/01.FirstReverse/solve.rb
@@ -0,0 +1,13 @@
+﻿def FirstReverse(str)
+	puts str.length
+	l = str.length - 1
+	result = ""
+
+	(0..l).each do |i|
+		result << str[l - i]
+	end
+
+	return result
+end
+
+puts FirstReverse(STDIN.gets)
---
+++ Easy/03.LongestWord/solve.rb
@@ -0,0 +1,17 @@
+﻿def LongestWord(sen)
+
+	# 1. extract all the words
+	# 2. assume words[0] has the biggest length, try to find bigger.
+	words = sen.split(/\W+/)
+	longest = words[0]
+
+	(1..words.length-1).each do |i|
+		if words[i].length > longest.length
+			longest = words[i]
+		end	
+	end
+
+	return longest
+end
+
+puts(LongestWord(STDIN.gets))
