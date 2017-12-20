f35e9b434f66d903398785534bf816c17cd3212a
If Beli <bgvladedivac@gmail.com>
2017-12-20 15:19:24 +0200

    Update solve.rb

--- Easy/07.SimpleSymbols/solve.rb
+++ Easy/07.SimpleSymbols/solve.rb
@@ -1,7 +1,16 @@
 ﻿def SimpleSymbols(str)
-
   # code goes here
-  return str 
+  # 1. check if char is letter 
+  # 2. compare
+  (1..str.length-1).each do |i|
+  	 
+  	if str[i] =~ /[[:alpha:]]/
+  		 if str[i-1] != '+' or str[i+1] != '+'
+  		 	return "false"
+  		 end
+  	end
+  end
+     "true" 
          
 end
 
