ffe190c93938f9b39b6f9624e27f2a150d33c73d
If Beli <bgvladedivac@gmail.com>
2018-01-12 13:03:39 -0800

    first hard one.

---
+++ Hard/01.PatternChaser/solve.py
@@ -0,0 +1,19 @@
+﻿def PatternChaser(s):
+	occurences = {}
+	
+	for i in range(0, len(s)-1):
+		check = s[i]
+		for j in range(i+1, len(s)):
+			check += s[j]
+			
+			if s.count(check) >= 2:
+				occurences[check] = s.count(check)
+	
+	if len(list(occurences.keys())) == 0:
+		return "no null"
+		
+	for key in sorted(occurences, key = lambda l: len(l), reverse=True):
+		return "yes " + key
+    
+# keep this function call here  
+print PatternChaser(raw_input())
